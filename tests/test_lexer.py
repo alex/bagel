117b535d0246649fda436e230cd9d6be28035332
Alex Gaynor <alex.gaynor@gmail.com>
2014-10-17 14:25:41 -0700

    check for branch coverage and fix missed branch

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -30,3 +30,25 @@ class TestLexer(object):
             Token("NEWLINE", "\n"),
             Token("DEDENT", ""),
         ])
+
+    def test_multiple_lines_at_indentation_level(self):
+        assert_lexes("""
+        def f():
+            return 3
+            return 4
+        """, [
+            Token("DEF", "def"),
+            Token("NAME", "f"),
+            Token("LPAREN", "("),
+            Token("RPAREN", ")"),
+            Token("COLON", ":"),
+            Token("NEWLINE", "\n"),
+            Token("INDENT", "    "),
+            Token("RETURN", "return"),
+            Token("INTEGER", "3"),
+            Token("NEWLINE", "\n"),
+            Token("RETURN", "return"),
+            Token("INTEGER", "4"),
+            Token("NEWLINE", "\n"),
+            Token("DEDENT", ""),
+        ])
