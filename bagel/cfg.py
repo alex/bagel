d7df3f51b59cf88a782e4d301a84206e22413179
Alex Gaynor <alex.gaynor@gmail.com>
2014-11-25 11:16:46 -0600

    Removed an unneeded level of indirection

--- bagel/cfg.py
+++ bagel/cfg.py
@@ -30,8 +30,7 @@ class Namespace(object):
 
     def visit_function(self, node, arg):
         function = self.new_function(node._name, node._arguments)
-        builder = InstructionBuilder(function, function._entry_block)
-        builder.visit(node._body, None)
+        function._entry_block.visit(node._body, None)
 
 
 class Function(object):
@@ -49,84 +48,6 @@ class Function(object):
         result = "B{:d}".format(self._block_counter)
         self._block_counter += 1
         return result
-
-
-class InstructionBuilder(object):
-    def __init__(self, function, block):
-        self._function = function
-        self._block = block
-
-    def emit(self, instruction):
-        self._block.emit(instruction)
-
-    def exit(self, exit_condition):
-        self._block.exit(exit_condition)
-
-    def new_temporary(self):
-        return self._block.new_temporary()
-
-    def has_exit(self):
-        return self._block.has_exit()
-
-    def new_block(self):
-        return Block(self._function)
-
-    def use_block(self, block):
-        self._block = block
-
-    def visit(self, node, arg):
-        return node.visit(self, arg)
-
-    def _visit_list(self, items, arg):
-        for item in items:
-            self.visit(item, arg)
-
-    def visit_suite(self, node, arg):
-        self._visit_list(node._body, arg)
-
-    def visit_return(self, node, arg):
-        v = self.visit(node._value, arg)
-        self.exit(ReturnValue(v))
-
-    def visit_assignment(self, node, arg):
-        # TODO: handle the fact that this is an assignment context
-        v1 = self.visit(node._target, arg)
-        v2 = self.visit(node._value, arg)
-        self.emit(Instruction(Opcodes.ASSIGN, [v2], v1))
-
-    def visit_if(self, node, arg):
-        v = self.visit(node._condition, arg)
-        if_block = self.new_block()
-        else_block = self.new_block()
-
-        self.exit(ConditionalBranch(v, if_block, else_block))
-
-        self.use_block(if_block)
-        self.visit(node._if_body, arg)
-        # TODO: handle fallthroug block
-        assert self.has_exit()
-
-        self.use_block(else_block)
-        self.visit(node._else_body, arg)
-        assert self.has_exit()
-
-    def visit_binop(self, node, arg):
-        v1 = self.visit(node._lhs, arg)
-        v2 = self.visit(node._rhs, arg)
-        result = self.new_temporary()
-        op = {
-            "+": Opcodes.ADD,
-        }[node._op]
-        self.emit(Instruction(op, [v1, v2], result=result))
-        return result
-
-    def visit_name(self, node, arg):
-        # TODO: figure out if the name is a local, update the symbol table,
-        # etc.
-        return LocalName(node._value)
-
-    def visit_integer(self, node, arg):
-        return ConstantInt(node._value)
 
 
 class Block(object):
@@ -149,6 +70,62 @@ class Block(object):
 
     def new_temporary(self):
         return InstructionResult(self._function.new_temporary_name())
+
+    def new_block(self):
+        return Block(self._function)
+
+    def visit(self, node, arg):
+        return node.visit(self, arg)
+
+    def _visit_list(self, items, arg):
+        for item in items:
+            self.visit(item, arg)
+
+    def visit_suite(self, node, arg):
+        self._visit_list(node._body, arg)
+
+    def visit_return(self, node, arg):
+        v = self.visit(node._value, arg)
+        self.exit(ReturnValue(v))
+
+    def visit_if(self, node, arg):
+        v = self.visit(node._condition, arg)
+        if_block = self.new_block()
+        else_block = self.new_block()
+
+        self.exit(ConditionalBranch(v, if_block, else_block))
+
+        if_block.visit(node._if_body, arg)
+        # TODO: handle fallthroug block
+        assert if_block.has_exit()
+
+        else_block.visit(node._else_body, arg)
+        assert else_block.has_exit()
+
+    def visit_assignment(self, node, arg):
+        # TODO: handle the fact that this is an assignment context
+        v1 = self.visit(node._target, arg)
+        v2 = self.visit(node._value, arg)
+        self.emit(Instruction(Opcodes.ASSIGN, [v2], v1))
+
+    def visit_binop(self, node, arg):
+        v1 = self.visit(node._lhs, arg)
+        v2 = self.visit(node._rhs, arg)
+        result = self.new_temporary()
+        op = {
+            "+": Opcodes.ADD,
+        }[node._op]
+        self.emit(Instruction(op, [v1, v2], result=result))
+        return result
+
+    def visit_name(self, node, arg):
+        # TODO: figure out if the name is a local, update the symbol table,
+        # etc.
+        return LocalName(node._value)
+
+    def visit_integer(self, node, arg):
+        return ConstantInt(node._value)
+
 
 
 class Instruction(object):
